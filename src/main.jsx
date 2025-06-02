66c071c858ede809a95d2044745356835cf74443
Sair Barreto <sairgregori@gmail.com>
2025-06-02 20:29:56 -0300

    Actualizado

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,14 @@ import './styles/variables.css';
 import './styles/layout.css';
 import './styles/products.css';
 import './styles/cart.css';
+import './index.css';
+
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
